d0b2913453cd564c0a1b80624e7db5b0fa254bee
imreapr <imreapr@gmail.com>
2022-05-24 13:41:51 -0500

    full react functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,47 @@
-import React from "react";
-import Header from "./Header";
+import React, { useState } from "react";import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes"
+import CreateArea from "./CreateArea";
 
 
 function App() {
-    return (
-    <div>
-        <Header />
-        { notes.map((noteItem => {
-        return <Note 
-            key = {noteItem.key}
-            title = {noteItem.title}
-            content = {noteItem.content}
-        />
-        })) }
-        <Footer />
-    </div>)
+
+    const [notes, setNotes] = useState([]);
+
+
+    function addNote(note) {
+        setNotes(prevNotes => {
+           return [...prevNotes, note];
+        });
+    }
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+          return prevNotes.filter((noteItem, index) => {
+            return index !== id;
+          });
+        });
+      }
+    
+      return (
+        <div>
+          <Header />
+          <CreateArea onAdd={addNote} />
+          {notes.map((noteItem, index) => {
+            return (
+              <Note
+                key={index}
+                id={index}
+                title={noteItem.title}
+                content={noteItem.content}
+                onDelete={deleteNote}
+              />
+            );
+          })}
+          <Footer />
+        </div>
+      );
 }
 
 export default App;
